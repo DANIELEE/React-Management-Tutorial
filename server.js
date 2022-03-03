7830c6d4727d8282652013e6c64d4159c89081d9
DANIELLEE <sky11213@hanmail.net>
2022-03-03 11:07:55 +0900

    Add Customer API Loading View

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ res.send([
 {
 'id': 1,
 'image': 'https://placeimg.com/64/64/1',
-'name': '홍길동',
-'birthday': '961222',
-'gender': '남자',
+'name': '이승미',
+'birthday': '910409',
+'gender': '여자',
 'job': '대학생'
 },
 {
 'id': 2,
 'image': 'https://placeimg.com/64/64/2',
-'name': '나동빈',
-'birthday': '960508',
+'name': '이경태',
+'birthday': '900814',
 'gender': '남자',
 'job': '프로그래머'
 },
